aac4ac773395aa9a98efc411ef9e0cbdfc0f67f0
kameo <kameo.sj@gmail.com>
2023-07-03 14:07:51 +0900

    푸터 업데이트

--- src/layouts/MainFooter.vue
+++ src/layouts/MainFooter.vue
@@ -1,6 +1,32 @@
 <template>
 
-    <footer class="footer">footer test</footer>
+  <footer class="footer">
+      <div class="inner">
+          <div class="f_logo">
+              <a href="javascript:void(0);"><span class="blind">LG U+</span></a>
+          </div>
+          <div class="f_buttom">
+              <a href="javascript:void(0);">이용약관</a>
+              <a href="javascript:void(0);">개인정보처리방침</a>
+              <a href="javascript:void(0);">개인정보이용내역</a>
+              <a href="javascript:void(0);">이메일무단수집거부</a>
+              <a href="javascript:void(0);">명의도용방지서비스</a>
+              <a href="javascript:void(0);">불법스팸대응센터</a>
+          </div>
+          <div class="f_info">
+              <P><span>대표이사 황현식</span><span>사업자등록번호 220-81-39938</span></P>
+              <P>서울특별시 용산구 한강대로 32</P>
+              <P>통신판매신고 제 2015-서울용산-00481호</P>
+          </div>
+          <div class="f_copyright">
+              © LG Uplus Corp. All Rights Reserved.
+          </div>
+          <div class="f_sns">
+              <a href="javascript:void(0);" class="ico_youtube"><span class="blind">youtube</span></a>
+              <a href="javascript:void(0);" class="ico_instagram"><span class="blind">instagram</span></a>
+          </div>
+      </div>
+  </footer>
 
 </template>
 
