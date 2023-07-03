79308324468e9893d4c61b95da65acb5d91d5df2
miok <mosong@the-51.com>
2023-07-03 17:47:57 +0900

    tooltip 작업중

--- src/views/guide/popup.vue
+++ src/views/guide/popup.vue
@@ -7,55 +7,73 @@
       
       <div class="section">
         <h3 class="tit02">Popup Alert</h3>
-
         <div class="group_btn">
-            <button type="button" class="btn_sL_c01" id="btn_popup01" @click="$popup.open('#popup01', '#btn_popup01');">Alert</button>
+          <button type="button" class="btn_sL_c01" id="btn_popup01" @click="$popup.open('#popup01', '#btn_popup01');">Alert</button>
         </div>
-
       </div> <!-- // section -->
       
       <div class="section">
         <h3 class="tit02">Popup Normal</h3>
-
         <div class="group_btn">
-            <button type="button" class="btn_sL_c02" id="btn_popup02" @click="$popup.open('#popup02', '#btn_popup02');">Normal</button>
+          <button type="button" class="btn_sL_c02" id="btn_popup02" @click="$popup.open('#popup02', '#btn_popup02');">Normal</button>
         </div>
-
       </div> <!-- // section -->
       
       <div class="section">
         <h3 class="tit02">Popup Full</h3>
-
         <div class="group_btn">
-            <button type="button" class="btn_sL_c03" id="btn_popup03" @click="$popup.open('#popup03', '#btn_popup03');">Full</button>
+          <button type="button" class="btn_sL_c03" id="btn_popup03" @click="$popup.open('#popup03', '#btn_popup03');">Full</button>
         </div>
-
       </div> <!-- // section -->
       
       <div class="section">
         <h3 class="tit02">Popup notice</h3>
-
         <div class="group_btn">
-            <button type="button" class="btn_sL_c04" id="btn_noticePop" @click="$popup.open('#noticePop', '#btn_noticePop');">notice</button>
+          <button type="button" class="btn_sL_c04" id="btn_noticePop" @click="$popup.open('#noticePop', '#btn_noticePop');">notice</button>
         </div>
-
       </div> <!-- // section -->
+
+
+      <div class="section">
+        <v-btn @click="tooltip[0] = !tooltip[0]" id="test">tooltip</v-btn>
+          
+        <v-tooltip v-model="tooltip[0]" activator="#test" location="bottom">
+          Tooltippp
+        </v-tooltip>
+        
+
+
+        <v-tooltip text="데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요" bottom>
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props">Tooltip</v-btn>
+          </template>
+        </v-tooltip>
+
+        <!-- <v-tooltip v-model="tooltip[0]" location="bottom">
+          <template>
+          </template>
+          <span>Programmatic tooltip</span>
+        </v-tooltip> -->
+      </div>
       
+
+
+
       <div class="section">
         <h3 class="tit02">tooltip</h3>
 
         데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요
         <span class="tooltip">
-            <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
-            <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
+          <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
+          <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
         </span>
 
         <br><br><br><br>
 
         데이터 소진 시 유튜브 화질 1080p로
         <span class="tooltip">
-            <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
-            <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
+          <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
+          <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
         </span>
         원활히 볼 수 있어요
 
@@ -63,8 +81,8 @@
 
         
         <span class="tooltip">
-            <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
-            <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
+          <button type="button" aria-expanded="false"><span class="blind">툴팁</span></button>
+          <span class="info" role="tooltip">데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요</span>
         </span>
         데이터 소진 시 유튜브 화질 1080p로 원활히 볼 수 있어요
 
@@ -79,5 +97,10 @@
 
 export default {
   name: 'GuidePopup',
+  data(){
+    return {
+      tooltip: [false, false, false],
+    }
+  }
 }
 </script>
