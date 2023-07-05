5ab419f38a270c38b85ca3561a370cda86aaf6f0
kameo <kameo.sj@gmail.com>
2023-07-05 14:39:46 +0900

    업데이트

---
+++ src/views/guide/Form.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="container">
+    <div class="content" id="content">
+      <h2 class="title_nav">Guide</h2>
+
+      <div class="page_info">Form</div>
+      
+      <div class="section">
+        <h3 class="tit02">input</h3>
+          <!-- <v-text-field placeholder="고객명" variant="outlined" hint="Enter your password to access this website" clearable></v-text-field> -->
+          <v-text-field placeholder="고객명 필수" variant="outlined" clearable></v-text-field>
+
+
+
+      </div> <!-- // section -->
+
+    </div> <!-- // content -->
+  </div><!-- // container -->
+
+</template>
+
+<script>
+
+export default {
+  name: 'GuideTemplate',
+}
+</script>
--- src/views/guide/List.vue
+++ src/views/guide/List.vue
@@ -65,6 +65,7 @@
 
             </div> <!-- // section -->
 
+
             <div class="section">
                 <h3 class="tit02">line_list</h3>
 
@@ -184,6 +185,29 @@
                         </li>
                     </ul>
                 </div>
+								<div class="group_btn pc_hide mgt12">
+										<a href="javascript:void(0);" role="button" class="btn_sM_c05">더볼래요</a>
+								</div>
+
+								<div class="pagination pc_show">
+										<button type="button" class="first" aria-label="처음 페이지로 이동">처음 페이지로 이동</button>
+										<button type="button" class="prev" aria-label="이전 페이지로 이동">이전 페이지로 이동</button>
+										<ul>
+												<li><button type="button" aria-label="페이지 1">1</button></li>
+												<li class="on"><button type="button" aria-label="페이지 2" title="현재 페이지">2</button></li>
+												<li><button type="button" aria-label="페이지 3">3</button></li>
+												<li><button type="button" aria-label="페이지 4">4</button></li>
+												<li><button type="button" aria-label="페이지 5">5</button></li>
+												<li><button type="button" aria-label="페이지 6">6</button></li>
+												<li><button type="button" aria-label="페이지 7">7</button></li>
+												<li><button type="button" aria-label="페이지 8">8</button></li>
+												<li><button type="button" aria-label="페이지 9">9</button></li>
+												<li><button type="button" aria-label="페이지 10">10</button></li>
+										</ul>
+										<button type="button" class="next" aria-label="다음 페이지로 이동">다음 페이지로 이동</button>
+										<button type="button" class="last" aria-label="마지막 페이지로 이동">마지막 페이지로 이동</button>
+								</div>
+
             </div> <!-- // section -->
 
             <div class="section">
@@ -381,6 +405,11 @@
                     </ul>
                 </div>
             </div> <!-- // section -->
+
+						<div class="section">
+							<h3 class="tit03">이벤트와 관련 있는 요금제에요</h3>
+							<a href="/guide/swiper" target="_blank">swiper page로 이동 (/guide/swiper)</a>
+						</div>
 
             <div class="section">
                 <h3 class="tit02">None Data</h3>
